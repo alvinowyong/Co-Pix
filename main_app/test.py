37a66db9f199dc5dc83a422ed04430d5574b976f
Chen Yun <chenyun964@gmail.com>
2022-03-30 18:37:34 +0800

    Add scrollbar

--- main_app/test.py
+++ main_app/test.py
@@ -8,7 +8,7 @@ import os.path
 import re
 import math
 
-DIVIDER = 12
+DIVIDER = 10
 
 def red_led():
     # Call red LED here
@@ -20,10 +20,14 @@ def speech_to_text():
     clear_frame()
     print("start recording")
     # Call Speed to text function here
-    time.sleep(5)
+    time.sleep(2)
     clear_frame()
     # Recongized text goes here
+    # text = "The first little pig was very lazy."
+
     text = "The first little pig was very lazy. He abandon. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but the was somewhat lazy too and he built his house out of sticks."
+    
+    # text = "The first little pig was very lazy. He abandon. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but the was somewhat lazy too and he built his house out of sticks., The first little pig was very lazy. He abandon. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but the was somewhat lazy too and he built his house out of sticks. "
     render_text(text)
     voice_indicate['text'] = "Complete"
 
@@ -35,16 +39,26 @@ def clear_frame():
 def render_text(text):
     words = text.split(" ")
     count = math.ceil(len(words)/DIVIDER) * 2
+
+    canvas = tk.Canvas(fr_text)
+    canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    vsb = tk.Scrollbar(fr_text, orient="vertical", command=canvas.yview)
+    vsb.pack(side=RIGHT, fill=Y)
+    canvas.config(yscrollcommand=vsb.set)
+
+    frame_words = tk.Frame(canvas)
+    canvas.create_window((0, 0), window=frame_words, anchor='nw')
+
     for i in range(count):
-        fr_text.columnconfigure(i, weight=1)
-        fr_text.rowconfigure(i, weight=1)
+        frame_words.columnconfigure(i, minsize=screen_width/DIVIDER)
+        frame_words.rowconfigure(i)
         
         for j in range(0, DIVIDER):
             frame = tk.Frame(
-                master=fr_text,
+                master=frame_words,
                 relief=tk.RAISED,
             )
-            frame.grid(row=i, column=j)
+            frame.grid(row=i, column=j, sticky=(N, S, E, W))
             index = int(i/2) * DIVIDER + j
             if(index >= len(words)):
                 continue
@@ -62,15 +76,20 @@ def render_text(text):
                     label = tk.Label(frame, image=icon)
                     label.image = icon
             label.pack(padx=5, pady=2)
-    # vsb = tk.Scrollbar(fr_text, orient="vertical")
-    # vsb.pack(side='right', fill='y')
+    frame_words.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
+    
 
 root = tk.Tk()
 root.title("Co-Pix")
 root.attributes('-fullscreen', True)
 
 content = tk.Frame(root)
-fr_text = tk.Frame(content, relief="ridge")
+fr_text = tk.Frame(content, relief="ridge", bg="white")
+
+#Get the current screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
 # Use this to view all fonts
 # print(font.families())
