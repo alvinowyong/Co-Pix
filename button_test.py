fa9c60006110f54748cca84797225d74e8e8476d
Jason Gu <jasongu9911@outlook.com>
2022-04-01 17:03:06 +0800

    update button test

--- button_test.py
+++ button_test.py
@@ -4,5 +4,11 @@ import main
 Question_Button = Button(2)
 Start_Button = Button(3)
 
+
+def press():
+    print("press me")
+    return 1
+
+
 while 1:
-    Start_Button.when_pressed = main.main_app()
+    Start_Button.when_pressed = press()
